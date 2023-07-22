af706da9b5f69cd7f727b66d202c6c7577cd1cd9
ArataShougou <aratashougou16@gmail.com>
2023-07-22 11:34:17 +0700

    Delete Local jquery

--- application/views/Header/header.php
+++ application/views/Header/header.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Help Desk - KKP</title>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo base_url();?>assets/css/datepicker3.css" rel="stylesheet">
 <link href="<?php echo base_url();?>assets/css/bootstrap-table.css" rel="stylesheet">
@@ -10,25 +11,15 @@
 
 <!--Icons-->
 <script src="<?php echo base_url();?>assets/js/lumino.glyphs.js"></script>
-
-
-<script src="<?php echo base_url();?>assets/js/jquery-1.11.1.min.js"></script>
-<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 <script src="<?php echo base_url();?>assets/js/chart.min.js"></script>
-<!-- <script src="<?php echo base_url();?>assets/js/chart-data.js"></script> -->
 <script src="<?php echo base_url();?>assets/js/easypiechart.js"></script>
-<script src="<?php echo base_url();?>assets/js/easypiechart-data.js"></script>
-<!-- <script src="<?php echo base_url();?>assets/js/bootstrap-datepicker.js"></script> -->
+<script src="<?php echo base_url();?>assets/js/easypiechart-data.js"></script>|
 <script src="<?php echo base_url();?>assets/js/bootstrap-table.js"></script>
-
 <script src="<?php echo base_url();?>assets/ckeditor-basic/ckeditor.js"></script>
 
 <link href="<?php echo base_url();?>assets/fontawesome/css/all.css" rel="stylesheet">
-
-
-
-
-
 
 <script>
 	!function ($) {
@@ -47,8 +38,6 @@
 </script>
 
 <script>
-
-
 	!function ($) {
 		$(document).on("click","ul.nav li.parent > a > span.icon", function(){          
 			$(this).find('em:first').toggleClass("glyphicon-minus");      
